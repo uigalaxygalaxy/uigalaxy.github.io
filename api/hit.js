ee9df6787a7325367c084303c16992f4d92fb6fa
uigalaxygalaxy <158004765+uigalaxygalaxy@users.noreply.github.com>
2024-11-30 22:40:52 -0500

    Update hit.js

--- api/hit.js
+++ api/hit.js
@@ -1,5 +1,6 @@
 import Redis from 'ioredis';
 
+
 export default async function handler(req, res) {
 
   // Initialize Redis connection
@@ -9,31 +10,38 @@ export default async function handler(req, res) {
 
   const RATE_LIMIT = 1; // Max number of requests per IP per hour
   const TIME_FRAME = 60 * 60;  // 1 hour in seconds
+  const MAX_LIMIT = 25;
   
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; // Get the IP address of the client
   
   const redisKey = `rate_limit:${ip}`;  // Redis key for the IP's request count
+  const redisTally = `total:${ip}`; //Keep tally of all IP's total connection count
+  
 
-  console.log(`Request received from IP: ${ip}`);
 
   // Check the current request count for the IP in Redis
-  const currentCount = await redis.get(redisKey);
-  console.log(`Current count for ${ip}: ${currentCount}`);
-
+  const [currentCount, TotalTally] = await Promise.all([
+    redis.get(redisKey),
+    redis.get(redisTally),
+  ]);
   // If the IP has exceeded the rate limit
   if (currentCount && parseInt(currentCount) >= RATE_LIMIT) {
-    console.log(`Rate limit exceeded for ${ip}`);
-    res.status(429).json({ error: 'Rate limit exceeded. Try again later.' });
+    res.status(429).json({ error: 'Rate limit exceeded.' });
+    return;
+  }
+
+  if (redisTally && parseInt(TotalTally) >= MAX_LIMIT) {
+    res.status(429).json({ error: 'Your IP has sent too many requests.'})
     return;
   }
 
   // Increment the request count and set expiration time for 1 hour
   await redis.multi()
     .incr(redisKey)  // Increment the request count
+    .incr(redisTally) // Tallies IP connection
     .expire(redisKey, TIME_FRAME)  // Set expiration time to 1 hour
     .exec();
 
-  console.log(`Incremented count for ${ip}, new count: ${currentCount + 1}`);
 
   // Handle CORS
   const allowedOrigins = [
