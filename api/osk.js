53c182e98ef1f8b16df0f33eed1374e3c51bba09
uigalaxygalaxy <158004765+uigalaxygalaxy@users.noreply.github.com>
2024-12-20 03:56:56 -0500

    Update osk.js

--- api/osk.js
+++ api/osk.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
         res.setHeader('Access-Control-Allow-Origin', origin);  // Dynamically set the origin
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
       }
     
       if (req.method === 'OPTIONS') {
