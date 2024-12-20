7402e6ee1762025713ef091400aabfc131034ab7
uigalaxygalaxy <158004765+uigalaxygalaxy@users.noreply.github.com>
2024-12-20 12:57:50 -0500

    Update oskhit.js

--- api/oskhit.js
+++ api/oskhit.js
@@ -43,6 +43,9 @@ export default async function handler(req, res) {
       res.setHeader('Access-Control-Allow-Credentials', 'true');
     }
     res.json('header2 ', origin);
+    res.json('host ', req.headers.host);
+    res.json('ref ', req.headers.referer);
+
     if (req.method === 'OPTIONS') {
       res.status(200).end();
       await redis.quit();
