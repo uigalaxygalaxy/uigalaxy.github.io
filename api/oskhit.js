5e92d335dbdf6454e0d5b647d678f5e24d6bc9d0
uigalaxygalaxy <158004765+uigalaxygalaxy@users.noreply.github.com>
2024-12-20 11:48:38 -0500

    testing

--- api/oskhit.js
+++ api/oskhit.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.setHeader('Access-Control-Allow-Credentials', 'true');
