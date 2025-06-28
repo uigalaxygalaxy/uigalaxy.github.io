bf399a75da8c2ff414e4d59c4408af3bf9d66376
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2025-06-28 00:56:29 -0400

    add css status

--- api/ribbon.js
+++ api/ribbon.js
@@ -6,10 +6,10 @@ export default async function handler(req, res) {
     if (req.method !== "GET") {
         return res.status(405).json({ error: "Method Not Allowed" });
     }
-
+    const { id } = req.query;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    const ribbonedKey = `ribboned:${ip}`;
-    const ribbonCountKey = "ribbonCount";
+    const ribbonedKey = `ribboned:${id}:${ip}`;
+    const ribbonCountKey = `ribbonCount:${id}`;
 
     try {
         const alreadyRibboned = await redis.get(ribbonedKey);
@@ -19,12 +19,12 @@ export default async function handler(req, res) {
             // Unribbon: remove the ribbon and decrement the count
             await redis.del(ribbonedKey);
             newCount = await redis.decr(ribbonCountKey);
-            return res.status(200).json({ message: "Unribboned!", ribbonCount: newCount });
+            return res.status(200).json({ message: "Unribboned!", ribbonCount: newCount, alreadyRibboned: false });
         } else {
             // Ribbon: set the ribbon and increment the count
             await redis.set(ribbonedKey, 1);
             newCount = await redis.incr(ribbonCountKey);
-            return res.status(200).json({ message: "Ribboned!", ribbonCount: newCount });
+            return res.status(200).json({ message: "Ribboned!", ribbonCount: newCount, alreadyRibboned: true });
         }
     } catch (err) {
         return res.status(500).json({ error: "Internal Server Error" });
