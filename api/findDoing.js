a490efa176ab8d8f9df2e994c346af135426dfac
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2025-09-17 23:05:28 -0400

    fix music stuff

--- api/findDoing.js
+++ api/findDoing.js
@@ -5,12 +5,13 @@ const redis = new Redis(process.env.REDIS_URL);
 export default async function handler(req, res) {
 const recent = await redis.get('recent') || "Offline"
 const songName = await redis.get('songName')
+const songArtist = await redis.get('songArtist')
 const songPlayer = await redis.get('songPlayer')
 const timestamp = Number(await redis.get('timestamp'))
 
 console.log(recent, songName, songPlayer, timestamp);
 
 res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ recent, songName, songPlayer, timestamp }));
+  res.end(JSON.stringify({ recent, songName, songPlayer, songArtist, timestamp }));
 
 }
