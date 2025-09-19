98b339ef36649bcc5d84e6d32e722e73741c4b96
uiGalaxyGalaxy <uigalaxyandromeda@gmail.com>
2025-09-18 21:39:43 -0400

    move updateTextTranslate() lower down so it claculates after the text is changed ooops

--- api/findDoing.js
+++ api/findDoing.js
@@ -9,7 +9,7 @@ const songArtist = await redis.get('songArtist')
 const songPlayer = await redis.get('songPlayer')
 const timestamp = Number(await redis.get('timestamp'))
 
-console.log(recent, songName, songPlayer, timestamp);
+console.log(recent, songName, songPlayer, songArtist, timestamp);
 
 res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify({ recent, songName, songPlayer, songArtist, timestamp }));
